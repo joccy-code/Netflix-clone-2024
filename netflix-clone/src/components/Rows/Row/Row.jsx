681f3d4936625570e41fd1d02e9d8c03c4d718dd
Jossy <jmesfin@ju2.edu.et>
2025-07-28 10:46:14 +0300

    Rows and RowLists

---
+++ netflix-clone/src/components/Rows/Row/Row.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./row.css";
+import axios from "../../../utils/axios";
+import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(`${fetchUrl}`);
+        console.log(request);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.error("Trailer not found:", error));
+    }
+  };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.backdrop_path : movie.poster_path
+            }`}
+            alt={movie.name}
+            onClick={() => handleClick(movie)}
+          />
+        ))}
+      </div>
+
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
